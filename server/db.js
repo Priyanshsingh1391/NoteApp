d9c7d570f0e75599e75aaac463346bbd52bfea66
priyanshsingh <singh.priyansh1391@gmail.com>
2025-06-22 23:21:09 +0530

    note app

---
+++ server/db.js
@@ -0,0 +1,14 @@
+// db.js
+const mongoose = require("mongoose");
+
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/Note_app");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error);
+  }
+};
+
+module.exports = connectToMongoDB;
